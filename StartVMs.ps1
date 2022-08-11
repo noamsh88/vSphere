c34b1f79ed2c23dbc326637b502132c59550afdf
Noam Shemesh <noam-s@novami.com>
2022-08-11 17:40:33 +0300

    add Start VMs script

---
+++ StartVMs.ps1
@@ -0,0 +1,53 @@
+﻿###############################################################################################
+#Script login to vCenter and will start single or multiple VMs according to argumentet entered#
+###############################################################################################
+$VMList = $args[0]
+$CredFilePath = "D:\vCenter\Credentials.xml"
+###############################################################################################
+
+# Validate if argument entered is not null
+if (!$VMList) {
+  $scriptName = $MyInvocation.MyCommand.Name
+  Write-Host "Usage:"
+  Write-Host "$scriptName <VM Name/List>"
+  Write-Host "e.g."
+  Write-Host "Single Host:      .\$scriptName VM1"
+  Write-Host "Multiple Hosts:   .\$scriptName VM1,VM2,VM3"
+  exit 1
+}
+
+Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -Confirm:$false
+
+# Create Connection to vCenter
+$vCenterName = '<vCenter URL>'
+$Credentials = Import-CliXml -Path "$CredFilePath"
+$VC_Connect = Connect-Viserver $vCenterName -Credential $Credentials
+
+foreach($vm in $VMList.split(','))
+{
+   Write-Host "Checking $vm VM"
+   # Validate if VM exist on vCenter
+   $Exists = Get-VM -name $vm -ErrorAction SilentlyContinue
+   If ($Exists){
+    Write-Host "Starting VM: $vm"
+   }
+   else {
+    Write-Host "$vm VM not found at $vCenterName vCenter, exiting.."
+    exit 1
+    }
+   
+
+   $VMPowerState = Get-VM -Name $vm | Select PowerState
+   Write-Host "$VMPowerState"
+
+   # Starting VM if it powered off
+   if ($VMPowerState -like "*PoweredOn*"){
+   Write-Host "$vm VM is already Powered ON"
+   }
+   else
+   {
+   Start-VM -VM $vm -Confirm:$false
+   }
+
+}
+
