4b2fcf5a5fcca9f9f6d3f0778c95807b05766921
Noam <noamsh88@gmail.com>
2022-08-14 10:49:21 +0300

    add Restart VMs script

---
+++ RestartVMs.ps1
@@ -0,0 +1,40 @@
+﻿#################################################################################################
+#Script login to vCenter and will restart single or multiple VMs according to argumentet entered#
+#################################################################################################
+$VMList = $args[0]
+$CredFilePath = "D:\vCenter\Credentials.xml"
+#################################################################################################
+
+# Validate if argument entered is not null
+if (!$VMList) {
+  $scriptName = $MyInvocation.MyCommand.Name
+  Write-Host "Usage:"
+  Write-Host "$scriptName <VM Name/List>"
+  Write-Host "e.g."
+  Write-Host "Single Host:      .\$scriptName VM1"
+  Write-Host "Multiple Hosts:   .\$scriptName VM1,VM2,VM3"
+  exit 1
+}
+
+Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -Confirm:$false
+
+# Create Connection to vCenter
+$vCenterName = '<vCenter URL>'
+$Credentials = Import-CliXml -Path "$CredFilePath"
+$VC_Connect = Connect-Viserver $vCenterName -Credential $Credentials
+
+
+foreach($vm in $VMList.split(','))
+{
+   # Validate if VM exist on vCenter
+   $Exists = Get-VM -name $vm -ErrorAction SilentlyContinue
+   If ($Exists){
+    Write-Host "Restarting VM: $vm"
+   }
+   else {
+    Write-Host "$vm VM not found at $vCenterName vCenter, exiting.."
+    exit 1
+    }
+
+   Restart-VM -VM $vm -Confirm:$false
+}
