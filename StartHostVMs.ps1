b73a46a95e6577c9830be3b8591601b2b1650503
noamsh88 <46564175+noamsh88@users.noreply.github.com>
2023-04-27 15:27:26 +0300

    Add start host VMs script

---
+++ StartHostVMs.ps1
@@ -0,0 +1,77 @@
+##############################################################################
+#Script login to vCenter and will start all VMs related to host name entered #
+##############################################################################
+$HostName = $args[0]
+$CredFilePath = "D:\vCenter\Credentials.xml"
+#############################################################################
+
+# Validate if argument entered is not null
+if (!$HostName) {
+  $scriptName = $MyInvocation.MyCommand.Name
+  Write-Host "Usage:"
+  Write-Host "$scriptName <Host/ESXi name>"
+  Write-Host "e.g."
+  Write-Host "pwsh $scriptName esxi-name-01"
+  exit 1
+}
+
+# Load VMware Core Module
+if (!(Get-Module -Name VMware.VimAutomation.Core) -and (Get-Module -ListAvailable -Name VMware.VimAutomation.Core))
+{
+    Write-Output "loading the VMware Core Module..."
+    Import-Module -Name VMware.VimAutomation.Core -ErrorAction Stop
+}
+
+Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -Confirm:$false
+
+# Create Connection to vCenter
+$vCenterName = '<vCenter URL>'
+$Credentials = Import-CliXml -Path "$CredFilePath"
+$VC_Connect = Connect-Viserver $vCenterName -Credential $Credentials
+
+# Validate Host name exist at vsphere
+$HostExists = Get-VMHost $HostName -ErrorAction SilentlyContinue
+if (!$HostExists) {
+  $scriptName = $MyInvocation.MyCommand.Name
+  Write-Host "$HostName Host NOT FOUND at $vCenterName vsphere, exiting.."
+  exit 1
+}
+
+# Get Host VM List
+$VMList = Get-VMHost $HostName | Get-VM | Select-Object Name
+
+# Start all Host VMs
+foreach($vm in $VMList)
+{
+   Write-Host "Checking $vm VM"
+   # Validate if VM exist on vCenter
+   $Exists = get-vm -name $vm -ErrorAction SilentlyContinue
+   If ($Exists){
+    Write-Host "Stopping VM: $vm"
+   }
+   else {
+    Write-Host "$vm VM not found at $vCenterName vCenter, skipping.."
+    Continue
+    }
+
+   # Starting VMs
+   Start-VM -VM $vm -Confirm:$false
+
+}
+
+# Validate all Host VMs are powered on
+foreach($vm in $VMList)
+{
+   # Get VM state (powered on/off)
+   $VMPowerState = Get-VM -Name $vm | Select PowerState
+   Write-Host "$VMPowerState"
+
+   # Exit with error in case VM is on power off state
+   if ($VMPowerState -like "*PoweredOff*"){
+     Write-Host " $vm VM on $HostName is Powered OFF, exiting.."
+     exit 1
+   }
+
+}
+
+Write-Host "$HostName VMs started succesfully" -BackgroundColor DarkGreen
